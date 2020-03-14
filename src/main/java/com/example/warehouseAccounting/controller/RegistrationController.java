e301800d783ed148c230bd07225a7a67b7656159
ArseniSakhnik <sakhnikarseni@mail.ru>
2020-03-14 18:11:42 +0300

    Commit

--- src/main/java/com/example/warehouseAccounting/controller/RegistrationController.java
+++ src/main/java/com/example/warehouseAccounting/controller/RegistrationController.java
@@ -1,16 +1,15 @@
 package com.example.warehouseAccounting.controller;
 import com.example.warehouseAccounting.domain.User;
+import com.example.warehouseAccounting.repos.UserRepo;
 import com.example.warehouseAccounting.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 
 import javax.validation.Valid;
-import java.util.Collections;
 
 @Controller
 public class RegistrationController {
@@ -19,31 +18,19 @@ public class RegistrationController {
     private UserService userService;
 
     @GetMapping("/registration")
-    public String registration(Model model)
+    public String registration()
     {
         return "registration";
     }
 
     @PostMapping("/registration")
-    public String addUser(@RequestParam String username,
-                          @RequestParam String password,
-                          Model model)
+    public String addUser(User user, Model model)
     {
-        User user = new User();
-        user.setUsername(username);
-        user.setPassword(password);
-
-        System.out.println("ПАРОЛЬ " + user.getUsername() + " ПАРОЛЬ " + user.getPassword());
-
-        if (!userService.saveUser(user)) {
-            model.addAttribute("message", "пользователь с таким именем уже существует");
+        if (!userService.saveUser(user)){
+            model.addAttribute("usernameError", "Пользователь с таким именем уже существует");
             return "registration";
         }
 
         return "redirect:/";
-
     }
-
-
-
 }
